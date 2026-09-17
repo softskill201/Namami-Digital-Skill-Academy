75d865d8f9feb765f6431daa223902d1b4c7cadb
softskill201 <softskill201@gmail.com>
2026-09-17 20:21:58 +0530

    Fix formatting of Supabase configuration variables

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-window.SUPABASE_URL = "https://satccuwqbhqrgffkflny.supabase.co";
-window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_MV4sxO_MplSB6EdtdxH9bQ_LLlLBiSD";
+window.SUPABASE_URL =
+"https://satccuwqbhqrgffkflny.supabase.co";
+
+window.SUPABASE_PUBLISHABLE_KEY =
+"sb_publishable_MV4sxO_MplSB6EdtdxH9bQ_LLlLBiSD";
